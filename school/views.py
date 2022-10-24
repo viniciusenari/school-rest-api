c05f76406e9631e9432f5a0c68ea633cdcef01e6
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-24 14:45:48 -0300

    feat: basic authentication

--- school/views.py
+++ school/views.py
@@ -1,27 +1,39 @@
 from rest_framework import viewsets, generics
 from school.models import Student, Course, Enrollment
 from school.serializer import StudentSerializer, CourseSerializer, EnrollmentSerializer, EnrollmentStudentListSerializer, StudentsEnrolledListSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class StudentsViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CoursesViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class EnrollmentsViewSet(viewsets.ModelViewSet):
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class EnrollmentStudentList(generics.ListAPIView):
     def get_queryset(self):
         queryset = Enrollment.objects.filter(student_id=self.kwargs['pk'])
         return queryset
     serializer_class = EnrollmentStudentListSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class StudentsEnrolledList(generics.ListAPIView):
     def get_queryset(self):
         queryset = Enrollment.objects.filter(course_id=self.kwargs['pk'])
         return queryset
     serializer_class = StudentsEnrolledListSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
