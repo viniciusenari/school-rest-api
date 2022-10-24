24e3c4ae4dfdc3fb2598b2992f66cc23ff9839d0
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-10-24 10:13:52 -0300

    feat: create students and courses view sets

--- school/views.py
+++ school/views.py
@@ -1,6 +1,11 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Student, Course
+from school.serializer import StudentSerializer, CourseSerializer
 
-def students(request):
-    if request.method == 'GET':
-        student = {'id': 1, 'name': 'John'}
-        return JsonResponse(student)
+class StudentsViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class CoursesViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
