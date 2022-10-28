10d4e3f5aab5e62efaefacea13a900e06d4cc18c
dzou <viniciusenari@gmail.com>
2022-10-28 15:03:50 -0300

    feat: add throttling

--- setup/settings.py
+++ setup/settings.py
@@ -133,4 +133,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/day',
+        'user': '500/day'
+    },
 }
